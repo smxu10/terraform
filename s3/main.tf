18291fd3c1c45e651a1ecbdb6d0b323bca922af2
sxu <smxu10@gmail.com>
2026-03-15 21:57:50 -0500

    aws oidc connectivity test

---
+++ s3/main.tf
@@ -0,0 +1,38 @@
+﻿variable "region" {
+  type    = string
+  default = "us-ease-1"
+}
+
+variable "bucket_name" {
+  type    = string
+  default = "seantest-bucket-1"
+}
+
+provider "aws" {
+  region = var.region
+}
+
+resource "aws_s3_bucket" "this" {
+  bucket = var.bucket_name
+
+  tags = {
+    Name = var.bucket_name
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "this" {
+  bucket = aws_s3_bucket.this.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+resource "aws_s3_bucket_ownership_controls" "this" {
+  bucket = aws_s3_bucket.this.id
+
+  rule {
+    object_ownership = "BucketOwnerEnforced"
+  }
+}
