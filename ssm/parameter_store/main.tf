8cc92b4d1bbdd11237604c8395fcc9dc9a655cc4
sxu <smxu10@gmail.com>
2026-05-13 13:28:28 -0500

    update Jenkinsfile

--- ssm/parameter_store/main.tf
+++ ssm/parameter_store/main.tf
@@ -1,12 +1,11 @@
 terraform {
-  required_version = "~> 1.5.0"
-
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.0"
+      version = "~> 4.31.0"
     }
   }
+  required_version = ">= 1.3.0"
 
   backend "s3" {
     bucket = "backend-sean-1"
